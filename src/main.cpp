fb3592d555b5cb7a2cba80808d763bef00f2de89
Dr-42 <royspandan1@gmail.com>
2022-07-02 03:43:53 +0530

    added Twinkle-Twinkle

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,11 @@
 #include "Wave.h"
 #include "Notes.h"
 
-void write(float* notes, int n){
+void write(float* notes, float* times, int n){
     Wave waves[n];
     for(int i = 0; i < n; i++)
     {
-        waves[i].init(notes[i], 0.5f, 0.9f);    
+        waves[i].init(notes[i], times[i], 0.5f);    
         if(i == 0){
             waves[i].writeWave();
         }else{
@@ -17,9 +17,10 @@ void write(float* notes, int n){
 }
 
 int main(){
-    float notes[] = { C5, C5, G5, G5, A5, A5, G5, G5 };
+    float notes[] = { C4, C4, G4, G4, A4, A4, G4, F4, F4, E4, E4, D4, D4, C4, G4, G4, F4, F4, E4, E4, D4, G4, G4, F4, F4, E4, E4, D4,C4, C4, G4, G4, A4, A4, G4, F4, F4, E4, E4, D4, D4, C4};
     int n = sizeof(notes)/ sizeof(float);
-    write(notes, n);
+    float times[] = {0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0,0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0,0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0};
+    write(notes, times, n);
     return 0;
 }
 
